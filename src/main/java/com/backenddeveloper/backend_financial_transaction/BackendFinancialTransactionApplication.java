d7378260f54adda82d1cef04416193505a39f716
Marcus Vinicius Felipe Rosa de Castro <vinicius@192.168.1.7>
2025-09-01 23:22:21 -0300

    projeto inicial

---
+++ src/main/java/com/backenddeveloper/backend_financial_transaction/BackendFinancialTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.backenddeveloper.backend_financial_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFinancialTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFinancialTransactionApplication.class, args);
+	}
+
+}
