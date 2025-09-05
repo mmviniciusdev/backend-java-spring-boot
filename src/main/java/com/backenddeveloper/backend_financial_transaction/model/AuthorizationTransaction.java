95ebd2caef9c0e4f71f38a5c3e4771f670a73000
mmviniciusdev <m.mvinicius.dev@gmail.com>
2025-09-04 22:22:05 -0300

    feat: Incluindo anotacao @JsonIgnore ignorando campos durante a serializacao (JAVA -> Json) e desserializacao (Json -> JAVA)

--- src/main/java/com/backenddeveloper/backend_financial_transaction/model/AuthorizationTransaction.java
+++ src/main/java/com/backenddeveloper/backend_financial_transaction/model/AuthorizationTransaction.java
@@ -3,6 +3,7 @@ package com.backenddeveloper.backend_financial_transaction.model;
 
 import com.backenddeveloper.backend_financial_transaction.dto.AuthorizationPOSRequestDTOCard;
 import com.backenddeveloper.backend_financial_transaction.dto.AuthorizationPOSRequestDTOInstalment;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import lombok.*;
@@ -13,17 +14,22 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@Entity(name = "transactions")
 @Table(name = "transactions")
 public class AuthorizationTransaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
     private String transactionId;
     private int amount;
     private String currency;
-    private String paymentType;
-    private String installment;
-    private String card;
+    private PaymentType paymentType;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "installment_id")
+    private Installment installment;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "creditCard_id")
+    private CreditCard card;
 }
