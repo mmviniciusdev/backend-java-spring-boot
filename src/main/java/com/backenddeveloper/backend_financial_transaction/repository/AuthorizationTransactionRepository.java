4377b2fa785f819c1db5d6354c1dd8d237ee3cf8
mmviniciusdev <m.mvinicius.dev@gmail.com>
2025-09-04 22:22:20 -0300

    feat: Construcao da camada de persistencia

--- src/main/java/com/backenddeveloper/backend_financial_transaction/repository/AuthorizationTransactionRepository.java
+++ src/main/java/com/backenddeveloper/backend_financial_transaction/repository/AuthorizationTransactionRepository.java
@@ -3,5 +3,8 @@ package com.backenddeveloper.backend_financial_transaction.repository;
 import com.backenddeveloper.backend_financial_transaction.model.AuthorizationTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorizationTransactionRepository extends JpaRepository<AuthorizationTransaction, Long> {
+
 }
